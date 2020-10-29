eb63ef9a7db9e25641056fe0cdb85f324cba913c
A1esha <72608100+A1esha@users.noreply.github.com>
2020-10-29 18:54:33 +0300

    Update main.ino
    возможный вариант

--- software/main/main.ino
+++ software/main/main.ino
@@ -1,9 +1,28 @@
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(D5,INPUT);
+pinMode(D2,INPUT);
+pinMode(D4,OUTPUT):
+pinMode(D3,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int t = digitalRead(D5);
+  int y = digitalRead(D2);
+  if(t>500){
+    digitalWrite(D4,HIGH);
+    delay(5000);
+    }
+    else{
+      digitalWrite(D4,LOW);
+      delay(5000);
+    }
+    if(y>25){
+      digitalWrite(D3,HIGH);
+      delay(5000);
+    }
+    else{
+      digitalWrite(D3,LOW);
+      delay(5000);
+    }
 }
